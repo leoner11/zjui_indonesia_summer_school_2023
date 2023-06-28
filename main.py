3ba6c2eac856bc20261a5c8207d54d73e16f2549
Chiming NI <chiming2@illinois.edu>
2023-06-28 22:14:29 +0800

    Upload main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import utils.vision as vision
+import utils.camera as camera
+import utils.serial as serial
+
+def main():
+    # Delete the following line and start coding!
+    pass
+
+if __name__ == '__main__':
+    main()
